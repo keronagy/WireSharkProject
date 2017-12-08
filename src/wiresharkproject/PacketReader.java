e29bb93db3e82603d8f5cd80746fc081cb142553
MinaWagdi <mina.wagdi63@gmail.com>
2017-12-08 18:48:47 +0200

    PacketDetails v1.1
    Just before running the project , you should load several web pages to capture packets.
    Thank YOU

--- src/wiresharkproject/PacketReader.java
+++ src/wiresharkproject/PacketReader.java
@@ -14,26 +14,32 @@ import jpcap.packet.Packet;
 public class PacketReader implements PacketReceiver{
     
     
-    void showDetailedView(Packet packet){
-        System.out.println("Length "+packet.caplen);
+    String showDetailedView(Packet packet){
+        String Packet="Packet: \n\n"+packet.toString()+"\n\n";
         
+        String Length = "Length : "+packet.caplen+"\n\n";
+        
+        String Data ="Data:\n\n";
         System.out.println("Data");
-        for(int i = 0; i<packet.data.length;i++){
-            System.out.print(packet.data[i]);
-        }
-        System.out.println("");
-        System.out.println("Header ");
-        for(int i = 0; i<packet.header.length;i++){
-            System.out.print(packet.header[i]);
-        }
-        System.out.println("");
-        System.out.println("Data Link \n"+packet.datalink.toString());
-        System.out.println("toString \n"+packet.toString());
+        for(int i = 0; i<packet.data.length;i++)
+            Data+=packet.data[i];
+        Data+="\n\n";
+        
+        
+        String Header = "Header:\n\n";
+        for(int i = 0; i<packet.header.length;i++)
+            Header+=packet.header[i];
+        Header+="\n\n";
+        
+        String DataLink = "Data Link : \n\n"+packet.datalink.toString()+"\n\n";
+        
+        System.out.println(Packet+Length+Header+Data+DataLink);
+        return Packet+Length+Header+Data+DataLink;
+        
     }
     //This Dummy
     public void receivePacket(Packet packet) {
-        System.out.println(packet);
-        showDetailedView(packet);
+        String s=showDetailedView(packet);
     }
     
   
