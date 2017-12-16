c68421e8101d0c9d1ac4549b09fe623403bbd9e3
Kord96 <k96.kord@gmail.com>
2017-12-17 00:34:48 +0200

    refactoring
    .

--- src/APIs/PacketReader.java
+++ src/APIs/PacketReader.java
@@ -25,25 +25,59 @@ import org.jnetpcap.protocol.tcpip.Udp;
 
 public class PacketReader {
     
-    public static int PacketNum=0;
-    private String PacketData;
-    private String PacketBytes;
-    private int[] featuresList;
-    private int packetSize;
-    private long FrameNumber;
-    private int PacketLength;
+       
+    //general data
     private Date datee;
-    private String protocol;
-    private int ID;
+    private String PacketBytes;
     private int DesPort;
     int SrcPort;
-    int DstPort;
-    int HeaderLength;
-    long ack;
-    long CheckSum;
-    long seqNum;
-    int Payload;
+    private String protocol;
+    private int length;
+    private String info;
+    private int ID;
     
+            //more details window
+                                            
+    //frame
+    private int InterfaceId;
+    private int EpochTime;
+    private long FrameNumber;
+    private int FrameLength;
+    
+    //ethernet
+    private String EthernetDest;
+    private String EthernetSource;
+    
+    //internet protocol version
+    private int IPversion;
+    private int HeaderLeangth;
+    private int IPid;
+    private boolean fragmentflag;
+    private int fragmentOffset;
+    private int timeToLive;
+    
+    
+    //tcp and udp
+    private int sourcePort;
+    private int DistinationPort;
+    private int checkSum;
+    //tcp only
+    private int seqNumber;
+    private int ackNumber;
+    private int windowSize;
+    
+    
+    
+    //http only
+    String host;
+    String ConnectionType;
+    String userAgent;
+    String AcceptEncoding;
+    
+    
+    //bonus
+    private int[] featuresList;
+
     private int HexaToDec(String s)
     {
         int val=0;        
@@ -70,9 +104,9 @@ public class PacketReader {
         Tcp tcp = new Tcp();
         
         PacketBytes =packet.toHexdump();      
-        packetSize = packet.size();
+        length = packet.size();
         FrameNumber = packet.getFrameNumber();
-        PacketLength = packet.getPacketWirelen();
+        //PacketLength = packet.getPacketWirelen();
         
         if(packet.hasHeader(http)){
             http= packet.getHeader(new Http());
@@ -80,7 +114,7 @@ public class PacketReader {
             ID = http.getId();
             //DesPort = http.getDescription();
             //SrcPort = ""+"";
-            HeaderLength = http.getHeaderLength();
+            //HeaderLength = http.getHeaderLength();
             //String MessageType="Message Type"+http.getMessageType();
             //String ContentType="Content Type : "+http.contentType();
             //String HeaderDescription= "Header Description"+http.getDescription();
@@ -90,26 +124,26 @@ public class PacketReader {
             tcp = packet.getHeader(new Tcp());
             protocol="TCP";
             ID = tcp.getId();
-            HeaderLength = tcp.getHeaderLength();
+            //HeaderLength = tcp.getHeaderLength();
             SrcPort = tcp.source();
-            DstPort= tcp.destination();
+            //DstPort= tcp.destination();
            // HeaderContentByte = "The content of the Header as a byte array : "+tcp.getHeader();
             //String Name="Name : "+tcp.getName();
-            ack = tcp.ack();
-            CheckSum = tcp.checksum();
-            HeaderLength = tcp.hlen();
-            seqNum = tcp.seq();
-              
+//            ack = tcp.ack();
+//            CheckSum = tcp.checksum();
+//            HeaderLength = tcp.hlen();
+//            seqNum = tcp.seq();
+//              
         }
         else if(packet.hasHeader(new Udp())){
             udp = packet.getHeader(new Udp());
             protocol = "UDP";
             ID = udp.getId();
-            HeaderLength = udp.getHeaderLength();
+//            HeaderLength = udp.getHeaderLength();
             SrcPort = udp.source();
             DesPort= udp.destination();
            // String HeaderContentByte = "The content of the Header as a byte array : "+udp.getHeader();
-            CheckSum = udp.checksum();
+//            CheckSum = udp.checksum();
             //Payload = udp.getPayload().toString();
             
             
