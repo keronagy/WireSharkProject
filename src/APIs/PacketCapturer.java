0d2bba155c91428d27391db25b93986803c049ca
Kord96 <k96.kord@gmail.com>
2017-12-17 22:20:01 +0200

    save .pcap added
    automatically saves no need to a button

--- src/APIs/PacketCapturer.java
+++ src/APIs/PacketCapturer.java
@@ -9,7 +9,11 @@ import GUI.RowPacket;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import org.jnetpcap.JBufferHandler;
 import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapDumper;
+import org.jnetpcap.PcapHeader;
+import org.jnetpcap.nio.JBuffer;
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
 import org.jnetpcap.protocol.tcpip.Http;
@@ -21,51 +25,57 @@ import org.jnetpcap.protocol.tcpip.Udp;
  * @author Kord
  */
 public class PacketCapturer {
+
     Pcap pcap;
     ArrayList PacketsStringList;
-    int [][] featuresList;
+    int[][] featuresList;
+
     public PacketCapturer() {
         PacketsStringList = new ArrayList();
         featuresList = new int[Constants.n][Constants.m]; //bonus stuff
     }
-    
-    public void StartCapture(int InterfaceIndex){
+
+    public void StartCapture(int InterfaceIndex) {
         OpenNetworkInterface(InterfaceIndex);
-        PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {  
+        
+        PcapDumper dumper = pcap.dumpOpen("saveFile.pcap");  
+        PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
             @Override
-            public void nextPacket(PcapPacket packet, String user) {  
+            public void nextPacket(PcapPacket packet, String user) {
                 PacketReader pr = new PacketReader();
                 pr.ReadPacket(packet);
-                 PacketsStringList.add(pr.getStringArray());
-                 //RowPacket.CreateRow(packet);
-             }  
-        };  
-        try{
-        pcap.loop(Pcap.LOOP_INFINITE, jpacketHandler, "");
-        }
-        catch(Exception e){System.out.println("Exception");};
+                dumper.dump(packet.getCaptureHeader(),packet);
+                PacketsStringList.add(pr.getStringArray());
+                //RowPacket.CreateRow(packet);
+            }
+        };
+
+        try {
+            pcap.loop(Pcap.LOOP_INFINITE, jpacketHandler, "");
+        } catch (Exception e) {
+            System.out.println("Exception");
+        };
     }
-    
-    public void OpenNetworkInterface(int InterfaceIndex){
+
+    public void OpenNetworkInterface(int InterfaceIndex) {
         StringBuilder errbuf = new StringBuilder();
         int snalen = 64 * 1024;           // Capture all packets, no truncation 
-            int promiscous = Pcap.MODE_PROMISCUOUS;
-           // InterfaceIndex = 2; //for kord
+        int promiscous = Pcap.MODE_PROMISCUOUS;
+        // InterfaceIndex = 2; //for kord
         int timeout = 60 * 1000; // In milliseconds
         String device = Constants.pc.getNetworkInterfacesList().get(InterfaceIndex).getName();
-        pcap=Pcap.openLive(device,snalen, promiscous, timeout, errbuf);
+        pcap = Pcap.openLive(device, snalen, promiscous, timeout, errbuf);
         //System.out.println(device);
-        
-        if(pcap==null){
+
+        if (pcap == null) {
             //System.out.println("Error when Opening the network interface with index : "+InterfaceIndex+"\nThe Errbuf is "+errbuf.toString());
         }
     }
-    
-    public void stopCapturing(){
-      
+
+    public void stopCapturing() {
+
         pcap.breakloop();
-    
+
     }
-    
-    
+
 }
